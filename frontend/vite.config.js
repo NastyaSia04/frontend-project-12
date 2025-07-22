443ade30d76f37aab72c13921e1325d707718ab1
NastyaSia04 <nastyasia04@gmail.com>
2025-07-22 16:05:42 +0200

    edited version vite and setted proxy in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5002, //явно указываем порт для фронтенда
+    proxy: {
+      //Прокси для REST API
+      '/api': {
+        target: 'http://localhost:5001', // Адрес сервера
+        changeOrigin: true,  // Рекомендуется для корректного CORS
+      },
+      // Прокси для WebSocket (если используется)
+      '/socket.io': {
+        target: 'ws://localhost:5001',  // WebSocket-адрес
+        ws: true,  // Обязательно для WebSocket
+        rewriteWsOrigin: true,  // Сохраняем, как в примере
+      },
+    },
+  },
 })
