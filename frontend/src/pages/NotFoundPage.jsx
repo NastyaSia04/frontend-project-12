9ff9e2bc0ff0148544a69a0e22acc416b16f32d5
NastyaSia04 <nastyasia04@gmail.com>
2025-07-27 15:14:11 +0200

    edited page 404

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>Страница не найдена</h1>
-      <p>Но вы можете <Link to='/'>перейти на главную страницу</Link></p>
+    <div className='h-100 d-flex flex-column align-items-center justify-content-start text-center bg-light'>
+      <img
+        src='/assets/404-D_FLHmTM.svg'
+        alt='Страница не найдена'
+        className='img-fluid h-25'
+        style={{ width: '291px', height: '291px' }}
+      />
+
+      <h1 className='h4 text-muted'>Страница не найдена</h1>
+      <p className='text-muted'>
+        Но вы можете <Link to='/'>перейти на главную страницу</Link>
+      </p>
     </div>
   )
 }
 
 export default NotFoundPage
-
-//адрес картинки src="https://cdn.hexlet.io/assets/error-page/not-found.svg"
