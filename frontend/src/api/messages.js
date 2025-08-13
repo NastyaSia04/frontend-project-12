6839e6a995195c2f52cc92907c339e7df3a5ef3e
NastyaSia04 <nastyasia04@gmail.com>
2025-08-13 19:14:46 +0200

    Удалить код тестовой ошибки в файле init.js

---
+++ frontend/src/api/messages.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { getAuthHeaders } from './headers';
+import { BASE_URL } from '../config'
+
+export const fetchMessages = () => {
+  return axios.get(`${BASE_URL}/messages`, getAuthHeaders())
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error fetching messages:', error)
+      throw error
+    })
+}
+
+export const sendMessage = (messageData) => {
+  return axios.post(`${BASE_URL}/messages`, messageData, getAuthHeaders())
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error sending message:', error)
+      throw error
+    })
+}
+
+export const deleteChannelMessages = (channelId) => {
+  return axios.delete(`${BASE_URL}/messages/channel/${channelId}`, getAuthHeaders())
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error deleting channel messages:', error)
+      throw error;
+    })
+}
+
+export default {
+  fetchMessages,
+  sendMessage,
+  deleteChannelMessages
+}
