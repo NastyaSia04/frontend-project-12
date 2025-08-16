82c64f98ed21eb6db6a982f57a60f934c82fb298
NastyaSia04 <nastyasia04@gmail.com>
2025-08-16 18:58:00 +0200

    Fix linter errors in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -13,12 +12,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/' element={<PrivateRoute><ChatPage /></PrivateRoute>} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <ToastContainer position='top-right' autoClose={3000}/>
+      <ToastContainer position="top-right" autoClose={3000} />
     </BrowserRouter>
   )
 }
